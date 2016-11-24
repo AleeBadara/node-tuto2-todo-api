8f921b3b02f9e0a5727e54fd1529c55b018f7f7d
Alioune Badara Mbaye <ambaye@csc.com>
2016-11-24 12:39:31 +0100

    Add Post /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,31 @@
+//Librairies exports
+var express = require('express');
+var bodyParser = require('body-parser'); //permet de transformer un json en objet javascript
+
+//Local exports
+var mongoose = require('./db/mongoose');
+var Todo = require('./models/todo');
+var User = require('./models/user');
+
+var app = express();
+
+//middleware
+app.use(bodyParser.json());
+
+//requête de création de todo
+app.post('/todos', function (req, res) {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then(function (doc) {
+        res.send(doc);
+    }, function (error) {
+        res.send(error);
+    });
+});
+
+
+app.listen(3000, function () {
+    console.log('Started on port 3000');
+});
