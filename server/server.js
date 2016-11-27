68d2d6541b259654a03a721e610ed3c4ff1b6440
Alioune Badara Mbaye <ambaye@csc.com>
2016-11-27 01:45:06 +0100

    Add DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -139,6 +139,15 @@ app.post('/users/login', function (req, res) {
     });
 });
 
+//DELETE /users/me/token
+app.delete('/users/me/token', authenticate, function (req, res) {
+    req.user.removeToken(req.token).then(function () {
+        res.status(200).send();
+    }, function () {
+        res.status(400).send();
+    });
+});
+
 
 
 
