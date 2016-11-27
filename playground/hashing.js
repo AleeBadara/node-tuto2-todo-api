d9c96668483e81447136d0ce44ab489ea41461e2
Alioune Badara Mbaye <ambaye@csc.com>
2016-11-27 01:02:29 +0100

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,22 @@
 const SHA256 = require('crypto-js').SHA256;
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123';
+
+/*bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+        console.log(hash);
+    });
+});*/
+
+var hashedPassword = '$2a$10$JxUBVaO3Zs/dNgUeRHJq1e5d1htbxCngO7XU758NKh37BSMccMdcq';
+
+bcrypt.compare(password, hashedPassword, function (err, res) {
+    console.log(res);
+});
+
+/*var data = {
     id: 20
 };
 
@@ -12,7 +27,7 @@ var decoded = jwt.verify(token, '123');
 console.log(decoded);
 
 var message = 'I am user number 1';
-var hash = SHA256(message).toString();
+var hash = SHA256(message).toString();*/
 
 /*console.log('Message', message);
 console.log('Hash', hash);
